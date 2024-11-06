5160a29f11e0e707fcc07531146eadbfdfb790f6
BATISTA-18 <joao.v.batista14@aluno.senai.br>
2024-11-06 10:41:48 -0300

    calculadora :codigo atualizado

---
+++ CalculadoraJoaoVitor/src/main/java/com/mycompany/calculadorajoaovitor/CalculadoraJoaoVitor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorajoaovitor;
+
+/**
+ *
+ * @author devmat
+ */
+public class CalculadoraJoaoVitor {
+
+    public static void main(String[] args) {
+         Tela_principal tela = new Tela_principal();
+        tela.setVisible(true);
+    }
+}
