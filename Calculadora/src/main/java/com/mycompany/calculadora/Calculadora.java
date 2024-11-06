e8305917460be1ac063addd1e5698b0b784bf0a9
BATISTA-18 <joao.v.batista14@aluno.senai.br>
2024-11-06 10:40:12 -0300

    projeto da calculadora

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.calculadora;
+
+import Interface_cal.Tela_principal;
+import View.Tela_principal;
+
+/**
+ *
+ * @author devmat
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Tela_principal tela = new Tela_principal();
+        tela.setVisible(true);
+    }
+}
